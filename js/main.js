16ef4ee414585f20bccfc2d71893406c3f645585
shvor <shvornev.dmitry@yandex.ru>
2021-05-29 01:25:08 +0300

    Добавляет вспомогательные функции

---
+++ js/main.js
@@ -0,0 +1,35 @@
+function getRandomDecimal(first, last) {
+  if (first < 0 || last < 0) {
+    return 0;
+  } else if (last < first) {
+    let swap = 0;
+    swap = last;
+    last = first;
+    first = swap;
+  } else if (last === first) {
+    return first;
+  }
+  let randomDecimal = 0;
+  randomDecimal = first + Math.random() * (last + 1 - first);
+  return Math.floor(randomDecimal);
+}
+
+function getRandomFloat(first, last, fractionDegree) {
+  if (first < 0.0 || last < 0.0 || fractionDegree < 0) {
+    return 0.0;
+  } else if (last < first) {
+    let swap = 0.0;
+    swap = last;
+    last = first;
+    first = swap;
+  } else if (last === first) {
+    return first;
+  }
+  let randomFloat = (first + Math.random() * (last - first));
+  randomFloat = randomFloat.toFixed(fractionDegree);
+  return randomFloat;
+}
+
+getRandomDecimal(1, 5);
+
+getRandomFloat(1.1, 6.6, 2);
