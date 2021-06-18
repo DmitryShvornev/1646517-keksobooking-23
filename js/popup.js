70066110a5a48d2e1ecdac5bd9ea9689ab845c77
shvor <shvornev.dmitry@yandex.ru>
2021-06-18 21:06:57 +0300

    Исправление стиля кода

--- js/popup.js
+++ js/popup.js
@@ -1,21 +1,12 @@
 import {createAd, ADS_NUMBER} from './data.js';
 
 const adTemplate = document.querySelector('#card').content.querySelector('.popup');
-const ads = new Array(ADS_NUMBER).fill(null);
-for (let number = 0; number < ads.length; number++) {
-  ads[number] = createAd(number + 1);
-}
-
+const ads = new Array(ADS_NUMBER).fill(null).map((_, index) => createAd(index + 1));
 const mapCanvas = document.querySelector('#map-canvas');
-const adElements = [];
 
-ads.forEach(({offer, author}) => {
-  const adElement = adTemplate.cloneNode(true);
-  adElement.querySelector('.popup__title').textContent = offer.title;
-  adElement.querySelector('.popup__text--address').textContent = offer.address;
-  adElement.querySelector('.popup__text--price').textContent = `${offer.price} ₽/ночь`;
-  let housingType;
-  switch (offer.type) {
+const getHousingTypeName = (type) => {
+  let housingType = '';
+  switch (type) {
     case 'flat':
       housingType = 'Квартира';
       break;
@@ -34,7 +25,15 @@ ads.forEach(({offer, author}) => {
     default:
       housingType = '';
   }
-  adElement.querySelector('.popup__type').textContent = housingType;
+  return housingType;
+};
+
+const renderOffer = ({offer, author}) => {
+  const adElement = adTemplate.cloneNode(true);
+  adElement.querySelector('.popup__title').textContent = offer.title;
+  adElement.querySelector('.popup__text--address').textContent = offer.address;
+  adElement.querySelector('.popup__text--price').textContent = `${offer.price} ₽/ночь`;
+  adElement.querySelector('.popup__type').textContent = getHousingTypeName(offer.type);
   adElement.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
   adElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
   const featureListElement = adElement.querySelector('.popup__features');
@@ -48,16 +47,17 @@ ads.forEach(({offer, author}) => {
   adElement.querySelector('.popup__description').textContent = offer.description;
   adElement.querySelector('.popup__photo').remove();
   const photosListElement = adElement.querySelector('.popup__photos');
-  for (let number = 0; number < offer.photos.length; number++) {
+  for (const element of offer.photos) {
     const photo = document.createElement('img');
     photo.width = 45;
     photo.height = 40;
-    photo.src = offer.photos[number];
+    photo.src = element;
     photo.classList.add('popup__photo');
     photosListElement.appendChild(photo);
   }
   adElement.querySelector('.popup__avatar').src = author.avatar;
-  adElements.push(adElement);
-});
+  return adElement;
+};
 
-mapCanvas.appendChild(adElements[0]);
+const renderOffers = (offers) => offers.map(renderOffer);
+mapCanvas.appendChild(renderOffers(ads)[0]);
