12d7307844e62ca2d32a18bcad1df3cd3e7d6276
shvor <shvornev.dmitry@yandex.ru>
2021-06-19 12:34:18 +0300

    Рефакторинг

--- js/form.js
+++ js/form.js
@@ -1,35 +1,28 @@
+const adForm = document.querySelector('.ad-form');
+const fieldsets = adForm.querySelectorAll('fieldset');
+const mapFilters = document.querySelector('.map__filters');
+const selects = mapFilters.querySelectorAll('select');
+
 const setInactive = () => {
-  const adForm = document.querySelector('.ad-form');
   adForm.classList.add('ad-form--disabled');
-  const fieldsets = adForm.querySelectorAll('fieldset');
   for (const fieldset of fieldsets) {
     fieldset.disabled = true;
   }
-  const mapFilters = document.querySelector('.map__filters');
   mapFilters.classList.add('map__filters--disabled');
-  const selects = mapFilters.querySelectorAll('select');
   for (const select of selects) {
     select.disabled = true;
   }
 };
 
-setInactive();
-
 const setActive = () => {
-  const adForm = document.querySelector('.ad-form');
   adForm.classList.remove('ad-form--disabled');
-  const fieldsets = adForm.querySelectorAll('fieldset');
   for (const fieldset of fieldsets) {
     fieldset.disabled = false;
   }
-  const mapFilters = document.querySelector('.map__filters');
   mapFilters.classList.remove('map__filters--disabled');
-  const selects = mapFilters.querySelectorAll('select');
   for (const select of selects) {
     select.disabled = false;
   }
 };
-
-setActive();
 
 export {setInactive, setActive};
