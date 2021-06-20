dd39211c778dfcd7a7911c06a21a9c89a2896bde
shvor <shvornev.dmitry@yandex.ru>
2021-06-21 01:39:07 +0300

    Начало валидации формы

--- js/form.js
+++ js/form.js
@@ -1,7 +1,23 @@
+const MIN_TITLE_LENGTH = 30;
+const MAX_TITLE_LENGTH = 100;
+const MAX_PRICE_VALUE = 1000000;
+const MIN_PRICES = {
+  'bungalow': 0,
+  'flat': 1000,
+  'hotel': 3000,
+  'house': 5000,
+  'palace': 10000,
+};
+
 const adForm = document.querySelector('.ad-form');
 const fieldsets = adForm.querySelectorAll('fieldset');
 const mapFilters = document.querySelector('.map__filters');
 const selects = mapFilters.querySelectorAll('select');
+const titleInput = adForm.querySelector('#title');
+const priceInput = adForm.querySelector('#price');
+const housingTypeSelect = adForm.querySelector('#type');
+const roomNumberSelect = adForm.querySelector('#room_number');
+const roomCapacitySelect = adForm.querySelector('#capacity');
 
 const setInactive = () => {
   adForm.classList.add('ad-form--disabled');
@@ -24,5 +40,59 @@ const setActive = () => {
     select.disabled = false;
   }
 };
+
+titleInput.addEventListener('input', () => {
+  const valueLength = titleInput.value.length;
+  if (valueLength < MIN_TITLE_LENGTH) {
+    titleInput.setCustomValidity(`Необходимо ещё ${ MIN_TITLE_LENGTH - valueLength } символов`);
+  } else if (valueLength > MAX_TITLE_LENGTH) {
+    titleInput.setCustomValidity(`Удалите лишние ${ valueLength - MAX_TITLE_LENGTH } символов`);
+  } else {
+    titleInput.setCustomValidity('');
+  }
+  titleInput.reportValidity();
+});
+
+priceInput.addEventListener('input', () => {
+  const value = Number(priceInput.value);
+  const housingType = housingTypeSelect.value;
+  if (value < MIN_PRICES[housingType]) {
+    priceInput.setCustomValidity(`Необходимо повысить цену на ${ MIN_PRICES[housingType] - value } руб./ночь`);
+  } else if (value > MAX_PRICE_VALUE) {
+    priceInput.setCustomValidity(`Необходимо понизить цену на ${ value - MAX_PRICE_VALUE } руб./ночь`);
+  } else {
+    priceInput.setCustomValidity('');
+  }
+  priceInput.reportValidity();
+});
+
+roomNumberSelect.addEventListener('input', () => {
+  const value = roomNumberSelect.value;
+  if (Number(value) === 1) {
+    roomCapacitySelect.querySelectorAll('option').forEach((item) => {
+      if (Number(item.value) !== 1) {
+        item.remove();
+      }
+    });
+  } else if (Number(value) === 2) {
+    roomCapacitySelect.querySelectorAll('option').forEach((item) => {
+      if (Number(item.value) > 2 || Number(item.value) < 1) {
+        item.remove();
+      }
+    });
+  } else if (Number(value) === 3) {
+    roomCapacitySelect.querySelectorAll('option').forEach((item) => {
+      if (Number(item.value) < 1) {
+        item.remove();
+      }
+    });
+  } else {
+    roomCapacitySelect.querySelectorAll('option').forEach((item) => {
+      if (Number(item.value) !== 0) {
+        item.remove();
+      }
+    });
+  }
+});
 
 export {setInactive, setActive};
